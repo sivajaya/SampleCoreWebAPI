652b50a7c10cde16360d4012e993aa638fa99bb8
Siva <Sivajaya75@gmail.com>
2022-01-25 18:13:44 +0530

    Env Change

--- SampleCoreAPI/Startup.cs
+++ SampleCoreAPI/Startup.cs
@@ -37,12 +37,12 @@ namespace SampleCoreAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleCoreAPI v1"));
-            }
+            //if (env.IsDevelopment())
+            //{
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleCoreAPI v1"));
+            //}
 
             app.UseHttpsRedirection();
 
